8c82901cab2fa9ec55186f91f5450e420add8106
Zets <zets.swe@gmail.com>
2016-01-20 22:29:38 +0300

    3rd

--- src/main/Main.java
+++ src/main/Main.java
@@ -31,6 +31,7 @@ public class Main {
         server.setHandler(handlers);
 
         server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
         server.join();
     }
 }
